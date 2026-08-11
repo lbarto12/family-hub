0c63dfe285d2a3bf31a2058c23b7a73cabbdfb10
Liam Barrack <lbarto12@hotmail.com>
2026-08-10 23:47:55 -0400

    fix type import for oRPC handlers

--- src/lib/client/linker.client.ts
+++ src/lib/client/linker.client.ts
@@ -1,8 +1,8 @@
 import { createORPCClient } from '@orpc/client';
 import { RPCLink } from '@orpc/client/fetch';
 
-import { router as publicRouter } from '$lib/server/rpc/public/router';
-import { router as privateRouter } from '$lib/server/rpc/private/router';
+import type { router as publicRouter } from '$lib/server/rpc/public/router';
+import type { router as privateRouter } from '$lib/server/rpc/private/router';
 import type { RouterClient } from '@orpc/server';
 
 const publicLink = new RPCLink({
