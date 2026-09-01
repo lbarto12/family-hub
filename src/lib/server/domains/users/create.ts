f01de407416809f042f3efc576bcb70723b4764e
Liam Barrack <lbarto12@hotmail.com>
2026-09-01 14:37:40 -0400

    export

--- src/lib/server/domains/users/create.ts
+++ src/lib/server/domains/users/create.ts
@@ -3,7 +3,7 @@ import { users, type User } from "$lib/server/db/schemas";
 import type { UserCreateRequest, UserResponse } from "$lib/types/rpcs/public/users/users";
 import { getFirst } from "$lib/server/domains/utils";
 
-export const Create = async (input: UserCreateRequest): Promise<UserResponse> => {
+export const New = async (input: UserCreateRequest): Promise<UserResponse> => {
     const passHash: string = await Bun.password.hash(input.password);
 
     const user: User = await db
---
+++ src/lib/server/domains/users/index.ts
@@ -0,0 +1,3 @@
+
+
+export * as create from "./create";
