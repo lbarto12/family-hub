6d0671cafdb12d892f248fdd102a5841413a1b8b
Liam Barrack <lbarto12@hotmail.com>
2026-09-04 18:43:59 -0400

    add error catch to sign in

--- src/routes/session/page.module.svelte.ts
+++ src/routes/session/page.module.svelte.ts
@@ -1,37 +1,42 @@
 import { API } from '$lib/client/linker.client';
 
 export interface Fields {
-	email: string;
-	password: string;
-	login: () => Promise<undefined>;
+    email: string;
+    password: string;
+    login: () => Promise<undefined>;
 }
 
 export const NewLoginPage = (): Fields => {
-	let email: string = $state('');
-	let password: string = $state('');
+    let email: string = $state('');
+    let password: string = $state('');
 
-	const login = async (): Promise<undefined> => {
-		const { access } = await API.public.session.login.Login({
-			email,
-			password
-		});
+    const login = async (): Promise<undefined> => {
+        try {
+            const { access } = await API.public.session.login.Login({
+                email,
+                password
+            });
+            API.setAccessToken(access);
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                console.log(e.message);
+            }
+        }
+    };
 
-		API.setAccessToken(access);
-	};
-
-	return {
-		set email(v: string) {
-			email = v;
-		},
-		get email() {
-			return email;
-		},
-		set password(v: string) {
-			password = v;
-		},
-		get password() {
-			return password;
-		},
-		login
-	};
+    return {
+        set email(v: string) {
+            email = v;
+        },
+        get email() {
+            return email;
+        },
+        set password(v: string) {
+            password = v;
+        },
+        get password() {
+            return password;
+        },
+        login
+    };
 };
